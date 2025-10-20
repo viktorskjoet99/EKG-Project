cfffba11e6f4754dae0597107ed1cd34d85a5d44
viktorskjot <201905373@post.au.dk>
2025-10-20 09:44:10 +0200

    Oprettet forbindelse til RaspberryPi

--- EKG_Project/RaspberryPi.cs
+++ EKG_Project/RaspberryPi.cs
@@ -1,6 +1,12 @@
+using System.Device.Gpio;
+using Iot.Device.Bindings;
+using Iot.Device.Mcp3xxx;
+using Iot.Device.Mcp3xxx.Mcp3208;
+
 namespace DefaultNamespace;
 
 public class RaspberryPi
 {
+	
     
 }
