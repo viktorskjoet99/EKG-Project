ee125000538e8d88c231dce1217171c1c2dd57cd
Macmand69 <201905373@post.au.dk>
2025-10-24 11:31:36 +0200

    Oprettet ECGProcessor og lavet ECGReadingConsumer

---
+++ EKG_Project/ECGProcessor.cs
@@ -0,0 +1,37 @@
+namespace EKG_Project;
+
+public class ECGProcessor
+{
+    private readonly object _lock = new object();
+    private readonly List<ECGSample> _samples = new();
+
+    public int Count()
+    {
+        lock (_lock)
+        {
+            return _samples.Count;
+        }
+    }
+
+    public void AddSample(ECGSample sample)
+    {
+        lock (_lock)
+        {
+            _samples.Add(sample);
+        }
+    }
+
+    public void ProcessSamples()
+    {
+        List<ECGSample> snapshot;
+        lock (_lock)
+        {
+            snapshot = new List<ECGSample>(_samples);
+        }
+
+        foreach (var sample in snapshot)
+        {
+            Console.WriteLine($"Value: {sample.Value}, TimeStamp: {sample.TimeStamp}");
+        }
+    }
+}
--- EKG_Project/ECGReadingConsumer.cs
+++ EKG_Project/ECGReadingConsumer.cs
@@ -1,6 +1,26 @@
+using System.Collections.Concurrent;
+
 namespace EKG_Project;
 
 public class ECGReadingConsumer
 {
+    private readonly BlockingCollection<ECGSample> _dataqueue;
+    private readonly ECGProcessor _processor;
+    private readonly ECGSample _sensor;
+
+    public ECGReadingConsumer(BlockingCollection<ECGSample> dataqueue, ECGSample sensor, ECGProcessor processor)
+    {
+        _dataqueue = dataqueue;
+        _sensor = sensor;
+        _processor = processor;
+    }
+
+    public void Run()
+    {
+        foreach (var sample in _dataqueue.GetConsumingEnumerable())
+        {
+            _processor.AddSample(sample);
+        }
+    }
     
 }
