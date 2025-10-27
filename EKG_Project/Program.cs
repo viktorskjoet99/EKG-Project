899e3920fc58b5dccf4b7d7e6eb18a306717e249
Macmand69 <201905373@post.au.dk>
2025-10-27 11:12:05 +0100

    Fjernet ligegyldige ting fra program.cs

--- EKG_Project/Program.cs
+++ EKG_Project/Program.cs
@@ -4,10 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("HEJ verden");
-        Console.WriteLine("HEJ verden2");
-        Console.WriteLine("HEJ verden3");
+       
 
         
         
