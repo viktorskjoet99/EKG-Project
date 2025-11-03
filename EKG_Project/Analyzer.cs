e6a0ccc73d571c76a5d0eb65697a83f26e30abb2
Macmand69 <201905373@post.au.dk>
2025-11-03 10:56:13 +0100

    Har lavet Datachunk, skal lige have testet det. Måske skal vi bruge metoden Analyze i Analyzer, da vi modtager dataen fra datachunk der i

--- EKG_Project/Analyzer.cs
+++ EKG_Project/Analyzer.cs
@@ -2,5 +2,9 @@ namespace EKG_Project;
 
 public class Analyzer
 {
-    
+
+    public void Analyze(List<ECGSample> samples)
+    {
+        // Her kan vi lave analyse af det modtaget chunks fra datachunks     
+    }
 }
--- EKG_Project/DataChunks.cs
+++ EKG_Project/DataChunks.cs
@@ -3,6 +3,39 @@ namespace EKG_Project;
 public class DataChunks
 {
     private readonly List<ECGSample> _chunks = new();
+    private readonly TimeSpan _interval = TimeSpan.FromMinutes(15);
+    private DateTime _lastUpdate = DateTime.MinValue;
     
+    private Analyzer _analyzer;
+
+    public DataChunks(Analyzer analyzer)
+    {
+        _analyzer = analyzer;
+    }
+
+    public void AddChunk(ECGSample chunk)
+    {
+        if (_lastUpdate == DateTime.MinValue)
+        {
+            _lastUpdate = chunk.TimeStamp;
+        }
+        _chunks.Add(chunk);
+
+        if (chunk.TimeStamp - _lastUpdate >= _interval)
+        {
+            FinalizeChunk();
+            _lastUpdate = chunk.TimeStamp;
+        }
+    }
+
+    private void FinalizeChunk()
+    {
+        if (_chunks.Count == 0) return;
+        
+        var finishedChunk = new List<ECGSample>(_chunks);
+        _chunks.Clear();
+        
+        _analyzer.Analyze(finishedChunk);
+    }
     
 }
