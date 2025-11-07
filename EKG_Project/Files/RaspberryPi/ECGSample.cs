cd25e062b62eeafac52884b7f3646e84e08f66a1
Macmand69 <201905373@post.au.dk>
2025-11-07 14:03:21 +0100

    Ændret Lead1 til int i stedet for double

--- EKG_Project/Files/RaspberryPi/ECGSample.cs
+++ EKG_Project/Files/RaspberryPi/ECGSample.cs
@@ -3,7 +3,7 @@ namespace EKG_Project;
 public class ECGSample
 {
     public int MeasurementId { get; set; }
-    public double Lead1 { get; set; }
+    public int Lead1 { get; set; }
     //public double Lead2 { get; set; }
     //public int lead3 { get; set; }
     public DateTime TimeStamp { get; set; }
