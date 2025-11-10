999dd1e00e615cf6d20d4262618cdce96bec068c
Macmand69 <201905373@post.au.dk>
2025-11-10 08:43:22 +0100

    Ændret fra short til int

--- EKG_Project/Files/RaspberryPi/FakeECGSensor.cs
+++ EKG_Project/Files/RaspberryPi/FakeECGSensor.cs
@@ -5,7 +5,7 @@ public class FakeECGSensor : IECGSensor
     private int _i = 0;
     private readonly Random _rand = new Random();
 
-    public short ReadRawSample()
+    public int ReadRawSample()
     {
         // Simulerer EKG-lignende bølgeform + tilfældig støj
         double v = 1000 * Math.Sin(_i * 0.02) + _rand.NextDouble() * 50 - 25;
