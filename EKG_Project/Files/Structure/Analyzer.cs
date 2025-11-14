4de0d209de0c0f9628606b63b6c47d69afd2e97b
Macmand69 <201905373@post.au.dk>
2025-11-14 12:49:22 +0100

    l

--- EKG_Project/Files/Structure/Analyzer.cs
+++ EKG_Project/Files/Structure/Analyzer.cs
@@ -16,7 +16,6 @@ public class Analyzer
     
     public void Analyze(List<ECGSample> samples)
     {
-        // Her kan vi lave analyse af det modtaget chunks fra datachunks   
         Console.WriteLine($"[Analyzer] Fik nyt chunk med {samples.Count} samples.");
         Console.WriteLine($"Første sample: {samples[0].TimeStamp}, Sidste sample: {samples[^1].TimeStamp}");
         
