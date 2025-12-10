a24587a1e5fd1cba7715ff58ed52c70fa53d2048
Macmand69 <201905373@post.au.dk>
2025-12-10 13:08:46 +0100

    små fejl, som ikke har betydning for koden

--- EKG_Project/Files/Business/AlarmSystem/AlarmToRelative.cs
+++ EKG_Project/Files/Business/AlarmSystem/AlarmToRelative.cs
@@ -5,6 +5,8 @@ public class AlarmToEmergencyContact : IAlarm
     public void Update(STStatus status)
     {
         if (status == STStatus.Elevation)
+        {
             Console.WriteLine("[EMERGENCY CONTACT] Critical heart event - Hospital has been notified");
+        }
     }
 }
