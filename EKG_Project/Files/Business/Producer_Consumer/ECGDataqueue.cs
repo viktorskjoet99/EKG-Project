be3b82e6020663ef7ba11ef114ac259e314eeb1e
Macmand69 <201905373@post.au.dk>
2025-12-10 13:16:21 +0100

    små ændringer

--- EKG_Project/Files/Business/Producer_Consumer/ECGDataqueue.cs
+++ EKG_Project/Files/Business/Producer_Consumer/ECGDataqueue.cs
@@ -11,19 +11,16 @@ public class ECGDataqueue
         _queue = new BlockingCollection<ECGSample>(boundedCapacity);
     }
     
-    // Producer kalder denne
     public void Add(ECGSample sample)
     {
         _queue.Add(sample);
     }
     
-    // Producer kalder denne når færdig
     public void CompleteAdding()
     {
         _queue.CompleteAdding();
     }
     
-    // Consumer kalder denne
     public IEnumerable<ECGSample> GetConsumingEnumerable()
     {
         return _queue.GetConsumingEnumerable();
